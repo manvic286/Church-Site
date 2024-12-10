948170334fbf538dfecd1093870c043835dd00fd
Manuel <manuelviotor@gmail.com>
2024-12-10 02:00:43 +0000

    Add files via upload

---
+++ models/Event.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    title: String,
+    date: Date,
+    time: String,
+    location: String,
+    description: String
+});
+
+module.exports = mongoose.model('Event', eventSchema); 
---
+++ routes/events.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const Event = require('../models/Event');
+
+// Get all events
+router.get('/', async (req, res) => {
+    const events = await Event.find();
+    res.render('events', { events });
+});
+
+// Create a new event
+router.post('/create', async (req, res) => {
+    const { title, date, time, location, description } = req.body;
+    const event = new Event({ title, date, time, location, description });
+    await event.save();
+    res.redirect('/events');
+});
+
+// Edit an event
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, date, time, location, description } = req.body;
+    await Event.findByIdAndUpdate(id, { title, date, time, location, description });
+    res.redirect('/events');
+});
+
+// Delete an event
+router.post('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    await Event.findByIdAndDelete(id);
+    res.redirect('/events');
+});
+
+module.exports = router; 
