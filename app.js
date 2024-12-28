b50db669f1187d1013d13c04fbc8f115a0136b6d
Manuel <manuelviotor@gmail.com>
2024-12-28 02:41:56 +0000

    changes to site operations

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-mongoose.connect('mongodb://localhost:27017/churchEvents', {
+mongoose.connect('mongodb+srv://manuelviotor:charleslwanga@charleslwanga.zj69b.mongodb.net/', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
@@ -22,6 +22,23 @@ const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+// Schemas
+const eventSchema = {
+    eventName: String,
+    month: String,
+    day: Number,
+    // date: Date,
+    // date: {
+    //     type: Date,
+    //     get: (date) => date.toLocaleDateString('en-US', { month: 'long', day: 'numeric' })
+    // },
+    time: String,
+    description: String
+    // imageUrl: String
+};
+
+const Event = mongoose.model('Event', eventSchema);
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -36,7 +53,11 @@ app.get('/services', (req, res) => {
 });
 
 app.get('/events', (req, res) => {
-    res.render('events');
+    Event.find()
+        .then(events => {
+            res.render('events', { events, title: "Events"})
+        })
+    // res.render('events');
 });
 
 app.get('/events/manage', (req, res) => {
